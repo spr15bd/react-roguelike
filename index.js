585bfb9b2075fee8eda792304912df8a256ddf9a
spr15bd <bevandady@gmail.com>
2021-05-08 00:00:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -106,8 +106,8 @@ class Data extends React.Component {
   	}
   	componentDidMount() {
 		
-    		//document.onkeypress = this.key.bind(this);
-		document.addEventListener("keydown", this.key);
+    		window.onkeypress = this.key.bind(this);
+		//document.addEventListener("keydown", this.key);
     		window.focus();
     		//this.displayMessage=this.displayMessage.bind(this);
     		//this.toggleDarkness=this.toggleDarkness.bind(this);
