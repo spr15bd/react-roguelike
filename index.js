ac8b9dccbabbbb44be49563a64ae9464132af591
spr15bd <bevandady@gmail.com>
2021-05-08 00:13:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ class Data extends React.Component {
     		window.scrollTo(0,0);
   	}
   	componentDidMount() {
-    		document.onkeydown = this.key.bind(this);
+    		window.onkeydown = this.key.bind(this);
 		//document.addEventListener("keydown", this.key);
     		window.focus();
     		this.displayMessage=this.displayMessage.bind(this);
