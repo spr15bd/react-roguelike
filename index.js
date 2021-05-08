12242bee8ec5fb92482ca80df7cb9f7c9319a2da
spr15bd <bevandady@gmail.com>
2021-05-08 01:30:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -384,7 +384,7 @@ class Data extends React.Component {
     		if (this.state.isDark) {
     			for (var x=0; x<this.props.mapWidth; x++) {
       				for (var y=0; y<this.props.mapHeight; y++) {
-        				if (x>=this.playerPos.x-this.props.flashlightRadius && x<this.playerPos.x+this.props.flashlightRadius+1 && y>=this.playerPos.y-this.props.flashlightRadius && y<this.playerPos.y+this.props.flashlightRadius+1) {
+        				if (x>=this.playerPos.x-this.props.flashlightRadius &&& x<this.playerPos.x+this.props.flashlightRadius+1 &&& y>=this.playerPos.y-this.props.flashlightRadius &&& y<this.playerPos.y+this.props.flashlightRadius+1) {
           					if (x>=0 && x<this.props.mapWidth && y>=0 && y<this.props.mapHeight) {
             						this.grid[x][y].visibility="visible";
           					}
