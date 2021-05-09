909bd88ffd4bb227eb17891814723f6fdd67e7c7
spr15bd <bevandady@gmail.com>
2021-05-09 23:34:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function Map(props) {
 					//squareId=props.grid[i][j].type;
 					//visibility=props.grid[i][j].visibility;
 					grid.push(
-						<div id="wall">
+						<div id={props.grid[i][j].type} className={props.grid[i][j].visibility}>
 						//<div id={props.grid[i][j].type} className={props.grid[i][j].visibility}>
 						</div>
 					);
