a6428cbb86ce158193a3f4b139a29c9a435b730e
spr15bd <bevandady@gmail.com>
2024-12-24 23:37:20 +0000

    Update index.js

--- index.js
+++ index.js
@@ -380,6 +380,7 @@ class Data extends React.Component {
     		if (this.isDark) {
     			for (let x=0; x<this.props.mapWidth; x++) {
       				for (let y=0; y<this.props.mapHeight; y++) {
+					// only update the gridpoint if gridpoint in view
         				if (x>=this.playerPos.x-this.props.flashlightRadius && x<this.playerPos.x+this.props.flashlightRadius+1 && y>=this.playerPos.y-this.props.flashlightRadius && y<this.playerPos.y+this.props.flashlightRadius+1) {
           					if (x>=0 && x<this.props.mapWidth && y>=0 && y<this.props.mapHeight) {
             						this.grid[x][y].visibility="visible";
