bccc8303c4b838bb616e9f5646b1c9e542cf2329
spr15bd <bevandady@gmail.com>
2025-01-01 23:52:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -410,6 +410,7 @@ class Data extends React.Component {
       			for (let x=0; x<this.props.mapWidth; x++) {
       				for (let y=0; y<this.props.mapHeight; y++) {
         				this.grid[x][y].isVisible = true;
+					console.log("ELSE - setting isVisible to: ", this.grid[x][y].isVisible);
       				}	
     			}
     		}
