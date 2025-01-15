6ea08b882e9d86281ebe0423b479e8582fa79649
spr15bd <bevandady@gmail.com>
2025-01-15 23:33:12 +0000

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ class Data extends React.Component {
       			message:"",
       			keyBoardLocked:false,
       			isDark:true,
-      			grid:[[[]]]
+      			grid:[[[]]]{ type: null, isVisible: null }
     		};
     		this.displayMessage=this.displayMessage.bind(this);
     		this.toggleDarkness=this.toggleDarkness.bind(this);
