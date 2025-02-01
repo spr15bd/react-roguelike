f9da315257ca5b8cddd8dbc1307645a3b140310c
spr15bd <bevandady@gmail.com>
2025-02-01 23:24:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -309,7 +309,7 @@ class Data extends React.Component {
           				break;
       			}
 			await this.updateDarkness();	// big pauses here
-			this.setState({grid:this.grid});
+			//this.setState({grid:this.grid});
     		}
     	}
   	movePlayer(x, y) {
