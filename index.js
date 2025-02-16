e3daae2247585511995d13f89b3c48507b11de63
spr15bd <bevandady@gmail.com>
2025-02-16 23:15:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -385,6 +385,7 @@ class Data extends React.Component {
 					inXRange = true;
 				} else {
 					inXRange = false;
+					this.grid[x][y].isVisible = false;
 				}	
       				for (let y=0; y<this.props.mapHeight; y++) {
 					// only update the gridpoint if gridpoint in view
