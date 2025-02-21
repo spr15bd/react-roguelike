3f877ed824a41c3d8fec803ada4a0fed0fc72b3f
spr15bd <bevandady@gmail.com>
2025-02-21 23:59:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -382,14 +382,14 @@ class Data extends React.Component {
       				for (let y=0; y<this.props.mapHeight; y++) {
         				if (x>=this.playerPos.x-this.props.flashlightRadius && x<this.playerPos.x+this.props.flashlightRadius+1 && y>=this.playerPos.y-this.props.flashlightRadius && y<this.playerPos.y+this.props.flashlightRadius+1) {
           					if (x>=0 && x<this.props.mapWidth && y>=0 && y<this.props.mapHeight) {
-            						this.grid[x][y].visibility="visible";
+            						this.grid[x][y].visibility=true;
           					}
         				} else {
-          					this.grid[x][y].visibility="invisible";
+          					this.grid[x][y].isVisible="invisible";
         				}
         				if ((x<=this.playerPos.x-(this.props.flashlightRadius-3)||x>=this.playerPos.x+(this.props.flashlightRadius-3))&&(y<=this.playerPos.y-(this.props.flashlightRadius-1)||y>=this.playerPos.y+this.props.flashlightRadius-1)||(y<=this.playerPos.y-(this.props.flashlightRadius-3)||y>=this.playerPos.y+(this.props.flashlightRadius-3))&&(x<=this.playerPos.x-(this.props.flashlightRadius-1)||x>=this.playerPos.x+(this.props.flashlightRadius-1))) {
           					if (x>=0 && x<this.props.mapWidth && y>=0 && y<this.props.mapHeight) {
-            						this.grid[x][y].visibility="invisible";
+            						this.grid[x][y].isVisible=false;
           					}
         				}
        				}
