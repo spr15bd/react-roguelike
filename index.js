f93711fb04e812bc167f04205239663d78e5bdf0
spr15bd <bevandady@gmail.com>
2025-03-10 23:57:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -383,13 +383,13 @@ class Data extends React.Component {
           					if (x>=0 && x<this.props.mapWidth && y>=0 && y<this.props.mapHeight) {
             						this.grid[x][y].isVisible=true;
           					}
-					} else {
-          					this.grid[x][y].isVisible=false;
-        				}
+					} //else {
+          				//	this.grid[x][y].isVisible=false;
+        				//}
         				if ((x<=this.playerPos.x-(this.props.flashlightRadius-3)||x>=this.playerPos.x+(this.props.flashlightRadius-3))&&(y<=this.playerPos.y-(this.props.flashlightRadius-1)||y>=this.playerPos.y+this.props.flashlightRadius-1)||(y<=this.playerPos.y-(this.props.flashlightRadius-3)||y>=this.playerPos.y+(this.props.flashlightRadius-3))&&(x<=this.playerPos.x-(this.props.flashlightRadius-1)||x>=this.playerPos.x+(this.props.flashlightRadius-1))) {
-          					//if (x>=0 && x<this.props.mapWidth && y>=0 && y<this.props.mapHeight) {
+          					if (x>=0 && x<this.props.mapWidth && y>=0 && y<this.props.mapHeight) {
             						this.grid[x][y].isVisible=false;
-          					//}
+          					}
         				}
        				}
     			}
