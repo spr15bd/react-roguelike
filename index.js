4bb335efa76bee393547c39d244f7d7c2ac74803
spr15bd <bevandady@gmail.com>
2025-03-15 23:45:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -379,18 +379,13 @@ class Data extends React.Component {
     		if (this.isDark) {
     			for (let x=0; x<this.props.mapWidth; ++x) {
       				for (let y=0; y<this.props.mapHeight; ++y) {
-        				if (y>=this.playerPos.y-this.props.flashlightRadius && y<this.playerPos.y+this.props.flashlightRadius+1) {
-          					if (x>=0 && x<this.props.mapWidth && y>=0 && y<this.props.mapHeight) {
-            						this.grid[x][y].isVisible=true;
-          					}
-					} else {
+        				if (x>=this.playerPos.x-this.props.flashlightRadius && x<this.playerPos.x+this.props.flashlightRadius+1 &&
+					   y>=this.playerPos.y-this.props.flashlightRadius && y<this.playerPos.y+this.props.flashlightRadius+1) {
+          					this.grid[x][y].isVisible=true;	
+        				} else {
           					this.grid[x][y].isVisible=false;
         				}
-        				if ((x<=this.playerPos.x-(this.props.flashlightRadius-3)||x>=this.playerPos.x+(this.props.flashlightRadius-3))&&(y<=this.playerPos.y-(this.props.flashlightRadius-1)||y>=this.playerPos.y+this.props.flashlightRadius-1)||(y<=this.playerPos.y-(this.props.flashlightRadius-3)||y>=this.playerPos.y+(this.props.flashlightRadius-3))&&(x<=this.playerPos.x-(this.props.flashlightRadius-1)||x>=this.playerPos.x+(this.props.flashlightRadius-1))) {
-          					if (x>=0 && x<this.props.mapWidth && y>=0 && y<this.props.mapHeight) {
-            						this.grid[x][y].isVisible=false;
-          					}
-        				}
+        				
        				}
     			}
     		} else {
