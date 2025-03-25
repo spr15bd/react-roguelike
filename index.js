0dfa3e8d8657510f581ed7376a57658e4d7270c6
spr15bd <bevandady@gmail.com>
2025-03-25 23:52:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -91,9 +91,10 @@ class Data extends React.Component {
     			The grid is passed to the <Map/> component
 		*/
 		console.log("Map height: "+this.props.mapHeight);
-    		this.grid=new Array(this.props.mapWidth);
+		this.grid = [[{ type: "", isVisible: false }]];
+    		
     		for (let x=0;x<this.props.mapWidth;++x) {
-			this.grid[x]=new Array(this.props.mapHeight);
+			//this.grid[x]=new Array(this.props.mapHeight);
 			for (let y=0;y<this.props.mapHeight;++y) {
 				this.grid[x][y].isVisible = false;
 			}
