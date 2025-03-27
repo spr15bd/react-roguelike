69d6da1dba9c59e7f6b09834a96466809cd78b55
spr15bd <bevandady@gmail.com>
2025-03-27 21:29:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -91,13 +91,13 @@ class Data extends React.Component {
     			The grid is passed to the <Map/> component
 		*/
 		console.log("Map height: "+this.props.mapHeight);
-		this.grid = [[{ type: "", isVisible: false }]];
-    		
-    		for (let x=0;x<this.props.mapWidth;++x) {
-			//this.grid[x]=new Array(this.props.mapHeight);
-			for (let y=0;y<this.props.mapHeight;++y) {
-				this.grid[x][y].isVisible = false;
-			}
+		for (let x=0;x<this.props.mapWidth;x++) {
+      			for (let y=0;y<this.props.mapHeight;y++) {
+        			this.grid[x][y]={
+          				type: "wall",
+          				isVisible: false
+        			};
+      			}
     		}
     		this.isDark = true;
     		this.loadDungeon();
