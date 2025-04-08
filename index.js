3b0f05ade04cfb82570aba47ed81c6533f1e6889
spr15bd <bevandady@gmail.com>
2025-04-08 23:50:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -93,12 +93,12 @@ class Data extends React.Component {
 		console.log("Map height: "+this.props.mapHeight);
     		//this.grid=new Array(this.props.mapWidth);
 		this.grid = [[{ type: "", isVisible: false }]]
-    		for (let x=0;x<this.props.mapWidth;++x) {
+    		/*for (let x=0;x<this.props.mapWidth;++x) {
 			//this.grid[x]=new Array(this.props.mapHeight);
 			for (let y=0;y<this.props.mapHeight;++y) {
 				this.grid[x][y].isVisible = false;
 			}
-    		}
+    		*/}
     		this.isDark = true;
     		this.loadDungeon();
     		window.scrollTo(0,0);
