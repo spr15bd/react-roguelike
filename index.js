f544f38f92734fd71e2a5038d631dca21a617eb5
spr15bd <bevandady@gmail.com>
2025-04-16 22:19:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -92,10 +92,11 @@ class Data extends React.Component {
 		*/
 		console.log("Map height: "+this.props.mapHeight);
     		//this.grid=new Array(this.props.mapWidth);
-		this.grid = [[{ type: "", isVisible: false }]];
+		//this.grid = [[{ type: "", isVisible: false }]];
+		this.grid = [];
 		console.log("initialising grid.....");
     		for (let x=0;x<this.props.mapWidth;++x) {
-			//this.grid[x]=new Array(this.props.mapHeight);
+			this.grid[x]=[];
 			for (let y=0;y<this.props.mapHeight;++y) {
 				this.grid[x][y] = { type: "", isVisible: false };
 			}
