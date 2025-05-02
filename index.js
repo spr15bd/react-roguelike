202e8389549eb0d530a93f6ff20a65c57160215d
spr15bd <bevandady@gmail.com>
2025-05-02 22:57:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -414,10 +414,9 @@ class Data extends React.Component {
   	}
 	async resetGrid() {
 		for (let x=0; x<this.props.mapWidth; x++) {
-      				for (let y=0; y<this.props.mapHeight; y++) {
-        				this.grid[x][y].isVisible = false;
-      				}	
-    			}
+      			for (let y=0; y<this.props.mapHeight; y++) {
+        			this.grid[x][y].isVisible = false;
+      			}
 		}
 	}
   	// display win or lose message, reload game
