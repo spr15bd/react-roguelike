08f9425e85b1a7d210b9a9accaef5c8404aa70c5
spr15bd <bevandady@gmail.com>
2025-05-24 23:58:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -413,7 +413,7 @@ class Data extends React.Component {
 		await this.updateMap();
   	}
 	async resetGrid() {
-		for (let x=0; x<this.props.mapWidth; ++x {
+		for (let x=0; x<this.props.mapWidth; ++x) {
       			for (let y=0; y<this.props.mapHeight; ++y) {
         			this.grid[x][y].isVisible = false;
       			}
